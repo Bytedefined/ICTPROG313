801aabc7d0595d88a8ee3e1451586ecc226a3e4f
Alexander <56295147+Bytedefined@users.noreply.github.com>
2024-05-17 11:23:04 +0800

    update

--- assets/scripts/game2.js
+++ assets/scripts/game2.js
@@ -1,7 +1,6 @@
 // Rock, paper, scissors
 let userChoice
 let computerChoice
-let userSelectedPaper, userSelectedRock, userSelectedScissors
 
 // Get user selection from pictures, being eiher rock, paper, or scissors
 document.getElementById("rock").addEventListener("click", userSelectedRock)
@@ -12,21 +11,21 @@ document.getElementById("scissors").addEventListener("click", userSelectedScisso
 function compare(choiceOne, choiceTwo) {
     // Note: choiceOne = user; choiceTwo = computer
     if (choiceOne === choiceTwo) {
-        document.getElementById("overallResult").innerHTML("It's a tie!")
+        document.getElementById("overallResult").innerHTML = "It's a tie!"
     } else if(choiceOne === "rock" && choiceTwo === "scissors") {
-        document.getElementById("overallResult").innerHTML("Rock beat scissors - you win!")
+        document.getElementById("overallResult").innerHTML = "Rock beat scissors - you win!"
     } else if(choiceOne === "scissors" && choiceTwo === "rock") {
-        document.getElementById("overallResult").innerHTML("Rock beat scissors - you lose!")
+        document.getElementById("overallResult").innerHTML = "Rock beat scissors - you lose!"
     } else if(choiceOne === "rock" && choiceTwo === "paper") {
-        document.getElementById("overallResult").innerHTML("Paper beat rock - you lose!")
+        document.getElementById("overallResult").innerHTML = "Paper beat rock - you lose!"
     } else if(choiceOne === "paper" && choiceTwo === "rock") {
-        document.getElementById("overallResult").innerHTML("Paper beat rock - you win!")
+        document.getElementById("overallResult").innerHTML = "Paper beat rock - you win!"
     } else if(choiceOne === "scissors" && choiceTwo === "paper") {
-        document.getElementById("overallResult").innerHTML("Scissors beat paper - you win!")
+        document.getElementById("overallResult").innerHTML = "Scissors beat paper - you win!"
     } else if(choiceOne === "paper" && choiceTwo === "scissors") {
-        document.getElementById("overallResult").innerHTML("Scissors beat paper - you lose!")
+        document.getElementById("overallResult").innerHTML = "Scissors beat paper - you lose!"
     } else {
-        document.getElementById("overallResult").innerHTML("Something went wrong - invalid values.")
+        document.getElementById("overallResult").innerHTML = "Something went wrong - invalid values."
     }
 }
 
@@ -35,16 +34,41 @@ function computerRoll() {
     computerChoice = Math.random()
 
     if(computerChoice < 0.3333) {
-        document.getElementById("compPick").innerHTML("Computer chose: Rock")
+        document.getElementById("compPick").innerHTML = "Computer chose: Rock"
         computerChoice = "rock"
+
         compare(userChoice, computerChoice)
     } else if(computerChoice <= 0.6667) {
-        document.getElementById("compPick").innerHTML("Computer chose: Paper")
+        document.getElementById("compPick").innerHTML = "Computer chose: Paper"
         computerChoice = "paper"
+
         compare(userChoice, computerChoice)
     } else {
-        document.getElementById("compPick").innerHTML("Computer chose: Scissors")
+        document.getElementById("compPick").innerHTML = "Computer chose: Scissors"
         computerChoice = "scissors"
+
         compare(userChoice, computerChoice)
     }
+}
+
+// Functions called to update userPick, sets userChoice and computer roll
+function userSelectedRock() {
+    document.getElementById("userPick").innerHTML = "You selected: Rock"
+    userChoice = "rock"
+
+    computerRoll()
+}
+
+function userSelectedPaper() {
+    document.getElementById("userPick").innerHTML = "You selected: Paper"
+    userChoice = "paper"
+
+    computerRoll()
+}
+
+function userSelectedRock() {
+    document.getElementById("userPick").innerHTML = "You selected: Scissors"
+    userChoice = "scissors"
+
+    computerRoll()
 }
